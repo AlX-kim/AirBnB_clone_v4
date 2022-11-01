c9f64663d9d33546d407c6aca644b27d431ba601
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2022-11-01 14:29:37 +0300

    version twoairbnb

---
+++ Desktop/AirBnB_clone_v2/models/amenity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Amenity module for HBNB project
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+
+
+class Amenity(BaseModel, Base):
+    """
+    This class Represents Amenities for a place
+
+    Inherits the BaseModel and Base(from sqlachemy)and links to the mysql
+    table amenities.
+    Attributes:
+    __tablename__(str): name of the MYSQL table
+        name(sqlalchemy string): name of the City
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
